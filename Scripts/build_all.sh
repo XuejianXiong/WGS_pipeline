32381efd8f21ddbc7ba142f4baa72b5abae99777
Xuejian Xiong <xiongxuejian@hotmail.com>
2025-10-14 10:18:04 -0400

    Update build_all.sh

--- Scripts/build_all.sh
+++ Scripts/build_all.sh
@@ -6,19 +6,19 @@ echo "Building base image..."
 docker build -t wgs-base:latest -f ../Docker/Dockerfile.base .
 
 echo "Building QC image..."
-docker build -t wgs-qc-reads:latest -f Docker/Dockerfile.qc_reads .
+docker build -t wgs-qc-reads:latest -f ../Docker/Dockerfile.qc_reads .
 
 echo "Building alignment image..."
-docker build -t wgs-align:latest -f Docker/Dockerfile.alignment .
+docker build -t wgs-align:latest -f ../Docker/Dockerfile.alignment .
 
 echo "Building fastp image..."
-docker build -t wgs-fastp:latest -f Docker/Dockerfile.fastp .
+docker build -t wgs-fastp:latest -f ../Docker/Dockerfile.fastp .
 
 echo "Building gatk image..."
-docker build -t wgs-gatk:latest -f Docker/Dockerfile.gatk .
+docker build -t wgs-gatk:latest -f ../Docker/Dockerfile.gatk .
 
 echo "Building bcftools image..."
-docker build -t wgs-bcftools:latest -f Docker/Dockerfile.bcftools .
+docker build -t wgs-bcftools:latest -f ../Docker/Dockerfile.bcftools .
 
 echo "✅ All Docker images built successfully!"
 
